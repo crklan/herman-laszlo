70dbc7683eb3aeaca9dec1b6098ba290933a2c61
Žan Peternelj <peternelj.zan@gmail.com>
2025-07-26 22:10:24 +0200

    fix: added border around images

--- app/components/ImagePreview.tsx
+++ app/components/ImagePreview.tsx
@@ -96,7 +96,7 @@ export const ImagePreview = ({
     >
       <img
         alt="Preview"
-        className="not-prose max-h-[500px] rounded-lg shadow-lg"
+        className="not-prose max-h-[500px] rounded-lg ring-4 ring-gray-400"
         src={builder.image(data.image).quality(40).fit('max').url()}
       />
     </Link>
